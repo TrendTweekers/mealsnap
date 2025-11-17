8afeb8080075af6047bd58194f2611fb68de5a47
Peter <trendtweakers00@gmail.com>
2025-11-17 11:55:21 +0100

    Fix Plausible domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,19 +49,9 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="MealSnap" />
         {/* Privacy-friendly analytics by Plausible */}
         <Script
-          strategy="afterInteractive"
-          src="https://plausible.io/js/pa-hbFlnDGOuevGYKH5ZEWB7.js"
-        />
-        <Script
-          id="plausible-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.plausible = window.plausible || function() { (plausible.q = plausible.q || []).push(arguments) };
-              plausible.init = plausible.init || function(i) { plausible.o = i || {} };
-              plausible.init();
-            `,
-          }}
+          defer
+          data-domain="mealsnap-chi.vercel.app"
+          src="https://plausible.io/js/script.js"
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
